45a8ed24a2b3af7c969c63edcd51548264910443
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-06-01 11:59:12 -0700

    add new metod to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,10 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates_length_of :password, 
-                          presence: true,
-                          minimum: 6
+  validates_length_of :password, presence: true, minimum: 6
 
   def authenticate_with_credentials(email, password)
-    if email == user.authenticate(password).email
-      User.new
-    else
-      nil
-    end
+    
   end
   
 end
